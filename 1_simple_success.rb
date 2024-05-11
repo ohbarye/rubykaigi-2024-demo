7a29c76c5bc9153139cc01702b1caa7e470dda2e
ohbarye <over.rye@gmail.com>
2024-05-11 17:53:05 +0900

    Update demo

--- 1_simple_success.rb
+++ 1_simple_success.rb
@@ -1,7 +1,7 @@
 require "pbt"
 
-# I know Ruby has Array#sort, but let's implement it ourselves
-# Note: This is not in-place quick sort
+# I know Ruby has Array#sort, but let's implement it ourselves.
+# Note: This is not in-place quick sort.
 def sort(array)
   return array if array.size <= 1
   pivot, *rest = array
@@ -9,15 +9,16 @@ def sort(array)
   sort(left) + [pivot] + sort(right)
 end
 
-# Example based testing
+# Example based testing looks like this:
 # RSpec.describe "sort" do
 #   it "sorts an array" do
 #     expect(sort([3, 1, 2])).to eq([1, 2, 3])
+#     expect(sort([3, 1])).to eq([1, 3])
+#     expect(sort([])).to eq([])
 #   end
 # end
 
 # Property based testing
-# Try with `worker: :ractor, :thread, :process`.
 Pbt.assert(worker: :ractor) do
   Pbt.property(Pbt.array(Pbt.integer)) do |numbers|
     result = sort(numbers)
--- 2_simple_failure.rb
+++ 2_simple_failure.rb
@@ -1,16 +1,15 @@
 require "pbt"
 
-# I know Ruby has Array#sort, but let's implement it ourselves
-# Note: This is not in-place quick sort
+# I know Ruby has Array#sort, but let's implement it ourselves.
+# Note: This is not in-place quick sort.
 def sort(array)
-  return array if array.size <= 2 # This is wrong!!!!!!!!!!!! It should be 1
+  return array if array.size <= 2 # This is wrong! It should be 1.
   pivot, *rest = array
   left, right = rest.partition { |n| n <= pivot }
   sort(left) + [pivot] + sort(right)
 end
 
-# Try with `verbose: true`.
-# Try with `seed: xxx`.
+# Check how the pbt reports the failure.
 Pbt.assert(verbose: true) do
   Pbt.property(Pbt.array(Pbt.integer)) do |numbers|
     result = sort(numbers)
